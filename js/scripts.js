35e52138acd8738cf52e3f0e0bfd4ea53598087b
Kassim Zaid <kzaid0767@gmail.com>
2022-03-07 12:09:07 -0500

    Week 4 Assignment

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,14 @@ $(function() {
         }
     });
 });
+
+$(document).ready(function() {
+    $("#reserveButton").click(function () {
+        $("#reserveModal").modal('show');
+    });
+
+$("#loginButton").click(function () {
+        $("#loginModal").modal('show');
+    });        
+});
+
